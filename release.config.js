60e63d1be25cf323352f00825a767379111bb983
Stoyan Yankov <stoyan.yankov@stendahls.se>
2023-02-05 11:29:46 +0200

    chore: Change configs

--- release.config.js
+++ release.config.js
@@ -3,13 +3,17 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/npm", {
-      "npmPublish": false,
-      "tarballDir": "dist",
-    }],
-    ["@semantic-release/github", {
-      "assets": "dist/*.tgz"
-    }]
+    '@semantic-release/npm',
+    '@semantic-release/github',
+
+
+    // ["@semantic-release/npm", {
+    //   "npmPublish": false,
+    //   "tarballDir": "dist",
+    // }],
+    // ["@semantic-release/github", {
+    //   "assets": "dist/*.tgz"
+    // }]
   ]
 };
 
