2485114e85774d1a21179aabe2300a245040a250
Stoyan Yankov <stoyan.yankov@stendahls.se>
2023-02-06 09:35:56 +0200

    Update the message

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const core = require('@actions/core');
 
 const person = core.getInput('person');
 
-console.log(`New greeting message !!!! ${person}!`);
+console.log(`New greeting message, update !!!! ${person}!`);
