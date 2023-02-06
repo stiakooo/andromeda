beac4349a2e1d3ca23c85b42bc35a1ad677b8507
stiakooo <stoyan.yankov@stendahls.se>
2023-02-06 10:57:44 +0200

    New update

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const core = require('@actions/core');
 
 const person = core.getInput('person');
 
-console.log(`New greeting message, update !!!! ${person}!`);
+console.log(`New greeting message, update, new update !!!! ${person}!`);
