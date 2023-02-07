988a0db1f040f293ee2139d86f8e274c65ffdcbd
stiakooo <stoyan.yankov@stendahls.se>
2023-02-07 11:43:58 +0200

    fix: add changelog to release config

--- release.config.js
+++ release.config.js
@@ -3,6 +3,7 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
 
